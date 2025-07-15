2a4e5633e880e2409f0fc2c04465cfced82fcbc6
tsikyFitia <tsikyfit@gmail.com>
2025-07-15 22:03:12 +0300

    creation containeur

---
+++ backend/app/database.py
@@ -0,0 +1,11 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = AsyncIOMotorClient(MONGO_URL)
+database = client["assco"]
+collection = database["users"]
